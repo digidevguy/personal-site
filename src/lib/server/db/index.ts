f636348250b5b27da7756f12d5f2464b795255c4
digidevguy <matthew@mattlittrell.dev>
2025-01-03 21:27:48 -0600

    Adjust env

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
-import { env } from '$env/dynamic/private';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const sql = neon(env.DATABASE_URL);
+import { DATABASE_URL } from '$env/static/private';
+if (DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const sql = neon(DATABASE_URL);
 export const db = drizzle(sql);
